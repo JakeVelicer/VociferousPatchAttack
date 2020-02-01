ca819eb2d60e60e085cc4a87b6269ae1b0db8932
JakeVelicer <007velicer@gmail.com>
2020-01-31 23:02:41 -0600

    Basic Movement

--- Vociferous Patch Attack/Assets/TruckMovement.cs
+++ Vociferous Patch Attack/Assets/TruckMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TruckMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public Rigidbody2D rigidBody;
+    public Transform truckSpriteObject;
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,15 @@ public class TruckMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement.x != 0 || movement.y != 0)
+        truckSpriteObject.transform.eulerAngles = new Vector3( 0, 0, Mathf.Atan2(-movement.x, movement.y) * 180 / Mathf.PI);
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
