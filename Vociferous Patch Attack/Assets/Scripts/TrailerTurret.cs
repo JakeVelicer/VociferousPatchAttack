0a1bf1a580ad7f96e918178d0fafa1c532d5c3c3
JakeVelicer <007velicer@gmail.com>
2020-02-01 20:33:04 -0600

    Turret Implemented

---
+++ Vociferous Patch Attack/Assets/Scripts/TrailerTurret.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerTurret : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletSpawnPoint;
+    public float rotateSpeed;
+    public float bulletSpeed;
+    public float fireRate;
+    private bool shooting;
+    private float currentTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("HorizontalTurret") < 0)
+        {
+            transform.Rotate(Vector3.forward * (rotateSpeed * Time.fixedDeltaTime));
+        }
+        else if (Input.GetAxisRaw("HorizontalTurret") > 0)
+        {
+            transform.Rotate(Vector3.forward * (-rotateSpeed * Time.fixedDeltaTime));
+        }
+
+        if (shooting)
+        {
+            currentTimer += Time.deltaTime;
+
+            if (currentTimer >= fireRate)
+            {
+                Shoot();
+                currentTimer = 0f;
+            }
+        }
+
+        if (Input.GetButtonDown("FireTurret") && !shooting)
+        {
+            shooting = true;
+        }
+        else if (Input.GetButtonUp("FireTurret"))
+        {
+            shooting = false;
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject newBullet = Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        newBullet.GetComponent<BulletScript>().SetBulletSpeed(bulletSpeed);
+    }
+}
